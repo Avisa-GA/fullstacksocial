fdfaf65b909a7183c424b2e640d53d59378d67de
Avisa-SEI <aap571420@gmail.com>
2021-06-27 14:17:35 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import React, {useState} from 'react';
 import './App.css';
+import LoginForm from './Components/LoginForm';
 
 function App() {
+
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+}
+
+const [user, setUser] = useState({name: "", email: ""});
+const [error, setError] = useState("");
+
+const login = details => {
+ console.log(details);
+ if (details.email === adminUser.email && details.password === adminUser.password ) {
+   console.log("logged in")
+   setUser({
+     name: details.name,
+     email: details.email
+   })
+ } else {
+   console.log("Details do not match")
+   setError("Details do not match")
+ }
+}
+
+const Logout = () => {
+   console.log('Logout');
+   setUser({name: "", email: ""})
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+{(user.email !== "") ? (
+
+      <div className="welcome">
+        <h2><span>{user.name}</span></h2>
+        <button className="waves-effect waves-light btn" onClick={Logout}>Logout</button>
+      </div>
+    ) : (
+      <LoginForm Login={login} error={error}/>
+    )}
+    
+      
     </div>
   );
 }
