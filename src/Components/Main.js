12667dcb69d2b38c393ce1c57c7592182b3f645e
Avisa-SEI <aap571420@gmail.com>
2021-06-27 18:28:43 -0700

    wip: add nav Routes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,10 @@ import React from "react";
 import Login from '../Pages/Login';
 import { Route, Switch } from "react-router-dom";
 import Signup from '../Pages/Signup';
+import Nav from './Nav';
+import Home from '../Pages/Home';
+import Search from '../Pages/Search';
+import Profile from '../Pages/Profile';
 
 export default function Main() {
 return (
@@ -10,6 +14,18 @@ return (
     <Switch>
         <Route exact path="/">
             <Login />
+        </Route>
+        <Route path="/posts">
+            <Nav />
+            <Route path="/posts/home">
+              <Home />
+            </Route>
+            <Route path="/posts/search">
+              <Search />
+            </Route>
+            <Route path="/posts/profile">
+              <Profile />
+            </Route>
         </Route>
         <Route to="/signup">
             <Signup />
