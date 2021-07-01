854eb4b39a9fd3e60d058c63b59d07a4bd91c0cb
Avisa-SEI <aap571420@gmail.com>
2021-07-01 16:29:59 -0700

    wip:

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,41 +4,73 @@ import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
 import PersonIcon from '@material-ui/icons/Person';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import { func } from "prop-types";
 
+const useStyles = makeStyles(theme => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Nav() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
 
-return (
-<div className="nav">
-  <nav className="pink darken-4">
-        
-    <div style={{marginLeft: "5%", marginRight: "5%"}} className="nav-wrapper fixed left">
-    <Link style={{marginLeft: "550%"}} className="brand-logo left"> 🚀 </Link>
-      <ul>
-       
-        <li>
-          <Link to="/" style={{marginLeft: "500%"}} className="brand-logo left">
-          <ExitToAppIcon />
-          </Link>
-        </li>
-        <li>
-          <Link to="/posts/home">
-          <HomeIcon />
-          </Link>
-        </li>
-        <li>
-          <Link style={{marginLeft: "150%"}} to="/posts/search">
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          className={classes.menuButton}
+        >
+          🚀
+        </IconButton>
+        <Typography className={classes.title}>
+        <Link to="/posts/home">
+         <HomeIcon />
+         </Link>
+        </Typography >
+        <Typography className={classes.title}>
+        <Link to="/posts/search">
           <SearchIcon />
-          </Link>
-        </li>
-        <li>
-          <Link style={{marginLeft: "300%"}} to="/posts/profile">
+           </Link>
+</Typography>
+<Typography className={classes.title}>
+<Link to="/posts/profile">
           <PersonIcon />
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-</div>
-);
+           </Link>
+</Typography>
+<Typography className={classes.title}>
+<Link to="/" >
+         <ExitToAppIcon />
+           </Link>
+</Typography>
+      </Toolbar>
+      
+    </AppBar>
+  );
 };
+
+
+
