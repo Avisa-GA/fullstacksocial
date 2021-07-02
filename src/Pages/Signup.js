28c0565a16ee6ee1fed00f66fabe5a91c84f727a
Avisa-SEI <aap571420@gmail.com>
2021-07-02 15:59:54 -0700

    wip: signout

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
+import {logout} from "../services/firebase";
 
-function Signup() {
+function Signup(props) {
 
 const [details, setDetails] = useState({name: "", email: "", password: ""});
 
@@ -26,7 +27,7 @@ return (
             setDetails({...details, password: e.target.value})} value={details.password}/>
         </div>
         <div style={{marginTop: "5%"}} className="submit">
-        <Link to="/posts" style={{width: "100%", marginTop: "5%", fontSize: "12px"}} className="waves-effect waves-light btn">Signup
+        <Link onClick={logout} to="/posts" style={{width: "100%", marginTop: "5%", fontSize: "12px"}} className="waves-effect waves-light btn">Signup
         </Link>
         </div>
     </form>
