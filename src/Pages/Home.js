3c3b00962713cb0ee0b79f876eff30b1e872cefd
Avisa-SEI <aap571420@gmail.com>
2021-07-03 10:43:23 -0700

    feat: Add user photo to each posts.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,56 +8,63 @@ import DeleteIcon from '@material-ui/icons/Delete';
 
 
 
-export default function Home({posts, deletePost, history}) {
- 
+export default function Home({posts, deletePost, history, user}) {
 
 // ************* Delete Action
 const handleDelete = id => {
-  deletePost(id);
-  history.push('/posts/home');
+deletePost(id);
+history.push('/posts/home');
 }
 
-  
+
 // ************************************************ Show
 const loading = () => {
- return (  <div className="preloader-wrapper active">
- <div className="spinner-layer spinner-red-only">
-   <div className="circle-clipper left">
-     <div className="circle"></div>
-   </div><div className="gap-patch">
-     <div className="circle"></div>
-   </div><div className="circle-clipper right">
-     <div className="circle"></div>
-   </div>
- </div>
+return ( <div className="preloader-wrapper active">
+  <div className="spinner-layer spinner-red-only">
+    <div className="circle-clipper left">
+      <div className="circle"></div>
+    </div>
+    <div className="gap-patch">
+      <div className="circle"></div>
+    </div>
+    <div className="circle-clipper right">
+      <div className="circle"></div>
+    </div>
+  </div>
 </div>
- );
+);
 };
 
 // Load function
 const loaded = () => {
-  return posts.map((post) => (
-    <ul className="collection">
-      <li className="collection-item">
-        {/* ************************* DELETE  */}
-        <button style={{backgroundColor: "white", borderStyle: "none", color: "rgb(228, 32, 88)", marginLeft: "95%"}} onClick={() => handleDelete(post._id)} ><DeleteIcon /></button>
-      {/* *********************************** */}
+return posts.map((post) => (
+<ul className="collection">
+  <li className="collection-item avatar">
+    {/* ********************** AVATAR CONTENT */}
+     <img src={user.photoURL} className="circle" />
+
+    {/* ************************* DELETE */}
+    <button style={{backgroundColor: "white", borderStyle: "none", color: "rgb(228, 32, 88)", marginLeft: "95%"}}
+      onClick={()=> handleDelete(post._id)} >
+      <DeleteIcon /></button>
+    {/* *********************************** */}
     <div key={post._id} className="post">
-    <p className="left-align" >{post.text}</p>
-    <div className="card-image center-align">
-      <img src={post.imageUrl}/>
+      <p className="left-align">{post.text}</p>
+      <div className="card-image center-align">
+        <img src={post.imageUrl} />
       </div>
     </div>
-    </li>
-    </ul>
-  ));
+  </li>
+</ul>
+));
 };
 
 
 return (
 <div style={{marginLeft: "20%", marginTop: "10%", width: "600px"}} className="home">
+
   { posts ? loaded() : loading() }
-  
+
 </div>
 );
 };
