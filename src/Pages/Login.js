907c90b8eccfee6ea3731d67ea1949122dc8bf8a
Avisa-SEI <aap571420@gmail.com>
2021-07-04 16:18:27 -0700

    wip:

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,31 +1,149 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-import {login} from '../services/firebase';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import {login, signUp, signOut, auth} from '../services/firebase';
+import axios from "axios";
 
+
+const URL = "https://social-full-backend.herokuapp.com/api/users"
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dzsyqjq3i/image/upload"
 
 export default function Login(props) {
+
+    // for signup or login
+    const [formMode, setFormMode] = useState({
+        loginEnabled: false
+    });
+
+    // creating a new form
+    const [formState, setFormState] = useState(newForm());
+
+    // user state
+    const [userSate, setUserState] = useState(null);
+
+    useEffect(() => {
+         const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                const {data} = await getLoggedInUser(user.uid);
+                setUserState(data);
+            } else {
+                setUserState(user);
+            }
+         });
+         return unsubscribe;
+    });
+
+    function getLoggedInUser(uid) {
+        return axios({
+            method: "GET",
+            url: URL + "/" + uid
+        });
+    }
+
+    async function handleSignup(e) {
+        e.preventDefault();
+        const {email, password, firstname, lastname} = formState;
+        try {
+            const {user} = await signUp(email, password);
+            let image;
+            
+            if (formState.image) {
+            // upload image to cloudinary
+                const data = new FormData();
+                data.append("file", formState.image);
+                data.append("upload_preset", "ml_default");
+            // fetch image from cloudinary
+             const res = await fetch(CLOUDINARY_URL, {
+                method: "POST",
+                body: data
+           });
+           image = await res.json();
+        }
+
+        const {data} = await axios({
+            url: URL + "signup",
+            method: "POST",
+            data: {
+                firstname,
+                lastname,
+                email,
+                firebaseUid: user.uid,
+                profileImageUrl: image ? image.secure_url : ""
+            }
+        });
+
+        setFormState(newForm());
+        setUserState(data);
+       
+        } catch ({message}) {
+            setFormState({ ...newForm(), errors: message });
+        }
+    }
+
+    async function handleLogin(e) {
+        e.preventDefault();
+        try {
+            const {email, password} = formState;
+            await login(email, password);
+            setFormState(newForm());
+        } catch ({message}) {
+            setFormState({ ...newForm(), errors: message });
+        }
+    }
+
+    function handleChange(e) {
+        setFormState((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+            errors: ""
+        }));
+    }
+
+    function handleImageFile(e) {
+        const file = e.target.files[0];
+        setFormState((prevState) => ({ ...prevState, image: file }));
+    }
+
+    function handleSignout() {
+           signOut();
+    }
+
+    function newForm() {
+        return {
+           email: "",
+           password: "",
+           firstname: "",
+           lastname: "",
+           image: null,
+           errors: ""
+        };
+    }
+
+    const {loginEnabled} = formMode;
+    const {email, password, firstname, lastname} = formState;
+
 return (
+  <>
+  {userSate && (
     <div className="login">
     <div style={{padding: "20%"}} className="card">
-    <h6 style={{fontWeight: "bold"}}>Log in to your account</h6>
+    <h6 style={{fontWeight: "bold", color: "rgb(38, 156, 143)", marginTop: "-2%"}}>Log in to your account</h6>
     <form>
         <div className="email">
-            <input id="email" type="email" class="validate" placeholder="Email" />
+            <input id="email" type="email" class="validate" placeholder="Email" style={{fontSize: 12}}/>
         </div>
         <div className="password">
-            <input id="password" type="password" class="validate" placeholder="Password" />
+            <input id="password" type="password" class="validate" placeholder="Password" style={{fontSize: 12}}/>
         </div>
         <div className="submit">
             <input style={{width: "100%", marginTop: "5%"}}  type="button" value="Submit" className="waves-effect waves-light btn" />
         </div>
         <div className="signup">
-            <p>Don't have an account? <Link to="/signup">Signup</Link></p>
+            <p style={{fontSize: 12}}>Don't have an account? <Link to="/signup">Signup</Link></p>
         </div>
 
     </form>
 </div>
 </div>
+  )}
+</>
 )};
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
-import {logout} from "../services/firebase";
 
 function Signup(props) {
 
@@ -10,24 +9,24 @@ const [details, setDetails] = useState({name: "", email: "", password: ""});
 
 return (
 
-    <div className="login">
+    <div className="signup">
     <div style={{padding: "20%"}} className="card">
     <h6 style={{fontWeight: "bold", marginBottom: "25%"}}>Create New Account</h6>
     <form>
-        <div className="username">
-        <input style={{fontSize: "12px"}} type="text" name="name" id="name" placeholder="Username" onChange={e=> setDetails({...details, name:
-            e.target.value})} value={details.name}/>
+        <div className="firstname">
+        <input style={{fontSize: "12px"}} type="text" name="firstname" placeholder="firstname" />
+        </div>
+        <div className="lastname">
+        <input style={{fontSize: "12px"}} type="text" name="lastname" placeholder="lastname" />
         </div>
         <div className="email">
-        <input style={{fontSize: "12px"}} type="text" name="email" id="email" placeholder="Email" onChange={e=> setDetails({...details, email:
-            e.target.value})} value={details.email}/>
+        <input style={{fontSize: "12px"}} type="text" name="email" placeholder="Email" />
         </div>
         <div className="password">
-        <input style={{fontSize: "12px"}} type="text" name="password" id="password" placeholder="Password" onChange={e=>
-            setDetails({...details, password: e.target.value})} value={details.password}/>
+        <input style={{fontSize: "12px"}} type="text" name="password" placeholder="Password" />
         </div>
         <div style={{marginTop: "5%"}} className="submit">
-        <Link to="/posts" style={{width: "100%", marginTop: "5%", fontSize: "12px"}} className="waves-effect waves-light btn">Signup
+        <Link to="/posts" style={{width: "100%", fontSize: "12px"}} className="waves-effect waves-light btn">Signup
         </Link>
         </div>
     </form>
