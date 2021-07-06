27f336e0b8839bb7f4c56b0924d9cb9a875b87e4
Avisa-SEI <aap571420@gmail.com>
2021-07-06 09:05:28 -0700

    feat: add post service

---
+++ src/services/post-service.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dzsyqjq3i/image/upload"
+const URL = "https://social-full-backend.herokuapp.com/post"
+
+
+async function getPosts(uid) {
+    const url = uid ? URL + '?uid=' + uid : URL;
+    const response = await fetch(url);
+    return response.json();
+};
+
+function uploadPostImage(data) {
+    return axios({
+      url: CLOUDINARY_URL,
+      method: "POST",
+      data
+    });
+  }
+
+
+async function createPost(post, token) {
+    return fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json",
+            "authorization": "bearer " + token
+        },
+        body: JSON.stringify(post)
+    })
+}
+
+async function deletePost(id, token) {
+    return fetch(URL + id, {
+        method: "DELETE",
+        headers: {
+            "authorization": "bearer " + token
+        }
+    });
+}
+
+export {getPosts, createPost, deletePost, uploadPostImage};
