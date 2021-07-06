0b59528843ed97f436493b3b2932fb32f07c1f98
Avisa-SEI <aap571420@gmail.com>
2021-07-05 19:11:40 -0700

    feat: signup page

---
+++ src/Pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import UserForm from "../Components/UserForm";
+
+
+export default function Signup(props) {
+    return (
+        <>
+        <UserForm isLogin={false}/>
+        </>
+    )
+}
