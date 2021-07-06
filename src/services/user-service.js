b863f16d5f405113dd15bea2289537cecf3caf19
Avisa-SEI <aap571420@gmail.com>
2021-07-05 19:11:29 -0700

    feat: add user-service

---
+++ src/services/user-service.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dzsyqjq3i/image/upload"
+const URL = "https://social-full-backend.herokuapp.com/"
+
+async function getLoggedInUser(user) {
+    const token = await user.getIdToken();
+    return axios({
+      method: "GET",
+      url: URL + "/login",
+      headers: {
+        authorization: "bearer " + token
+      }
+    });
+  }
+  
+  function uploadAvatar(data) {
+    return axios({
+      url: CLOUDINARY_URL,
+      method: "POST",
+      data
+    });
+  }
+  
+  function createUser(data, token) {
+    return axios({
+      url: URL + "/signup",
+      method: "POST",
+      headers: {
+        authorization: "bearer " + token
+      },
+      data
+    });
+  }
+  
+  export { getLoggedInUser, uploadAvatar, createUser };
+  
