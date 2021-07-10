4b14c4f798ebe96078038d1807162897ae07d36b
Avisa-SEI <aap571420@gmail.com>
2021-07-10 07:08:35 -0700

    feat: add follow/follwing service for users

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -8,6 +8,37 @@ async function allUsers(uid) {
    const response = await fetch(USER_URL);
    return response.json();
 }
+// *************************** Followers
+async function allFolowers(user) {
+  const token = await user.getIdToken();
+    const response = await fetch(USER_URL + "/" + user._id + "followers", {
+      method: "GET",
+      headers: {
+        "Content-Type": "Application/json",
+        "authorization": "bearer " + token
+      }
+    });
+    return response.json();
+}
+
+async function follow(id, token) {
+  return fetch(USER_URL + "/" + id + "/follow", {
+      method: "POST",
+      headers: {
+          "authorization": "bearer " + token
+      }
+  });
+}
+
+async function unfollow(id, token) {
+  return fetch(USER_URL + "/" + id + "/unfollow", {
+      method: "POST",
+      headers: {
+          "authorization": "bearer " + token
+      }
+  });
+}
+// *********************************
 
 async function getLoggedInUser(user) {
     const token = await user.getIdToken();
@@ -39,5 +70,5 @@ async function getLoggedInUser(user) {
     });
   }
   
-  export { getLoggedInUser, uploadAvatar, createUser, allUsers };
+  export { getLoggedInUser, uploadAvatar, createUser, allUsers, follow, unfollow, allFolowers };
   
